4a47d9e94145693426518a9ec8b4f32df025913d
Guil H <g.hernandez07@gmail.com>
2024-05-10 15:50:20 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 import { openai } from "./config.js";
 import "./style.css";
+
+const sessions = [
+  "AI and Education—Developing a Data Strategy",
+  "3D Printing with Clay",
+  "Art in the Age of Automation",
+  "Virtual and Augmented Reality",
+  "Diversity in the Maker Community",
+];
+
+async function main() {
+  const embedding = await openai.embeddings.create({
+    model: "text-embedding-3-small",
+    input: sessions,
+    encoding_format: "float",
+  });
+
+  console.log(embedding.data);
+}
+
+main();
