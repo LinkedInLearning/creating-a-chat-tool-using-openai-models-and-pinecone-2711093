0609781b817fa41c7cca7d31285a0f434bc71025
Guil H <g.hernandez07@gmail.com>
2024-05-10 16:50:58 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,50 +1,26 @@
 import { openai, pinecone, index } from "./config.js";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { nextId } from "./utils";
 import "./style.css";
 
-const txtFile = "./documents/sessions.txt";
+// DOM elements
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const chatReply = document.querySelector("#chat-reply");
 
-// Write vectors into Pinecone index
-async function upsertRecords() {
-  const embeddingsData = await generateEmbeddings(txtFile);
+// Submit form
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  input.value = "";
+});
 
-  await index.upsert(embeddingsData);
-  console.log("Upsert Successful!");
-}
-// upsertRecords();
-console.log(await index.describeIndexStats());
-
-// LangChain text splitter
-async function splitText(document) {
-  const response = await fetch(document);
-  const text = await response.text();
-
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 200,
-    chunkOverlap: 20,
+// Generate embedding from query
+async function generateEmbedding(input) {
+  const embeddingResponse = await openai.embeddings.create({
+    model: "text-embedding-3-small",
+    input,
   });
 
-  const output = await splitter.createDocuments([text]);
-  return output;
+  return embeddingResponse.data[0].embedding;
 }
 
-// Generate embeddings
-async function generateEmbeddings(doc) {
-  const data = [];
-  const text = await splitText(doc);
-
-  for (const textChunk of text) {
-    const embeddingResponse = await openai.embeddings.create({
-      model: "text-embedding-3-small",
-      input: textChunk.pageContent,
-    });
-    data.push({
-      id: nextId(),
-      values: embeddingResponse.data[0].embedding,
-      metadata: { content: textChunk.pageContent },
-    });
-  }
-  console.log("Embeddings complete!");
-  return data;
-}
+// Query Pinecone using embedding
+async function queryData(queryVector) {}
