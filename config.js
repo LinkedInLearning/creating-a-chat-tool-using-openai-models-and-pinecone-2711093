ba481a440029ad75048d7f323b71ffcc218dfff8
Guil H <g.hernandez07@gmail.com>
2024-05-10 20:23:37 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,14 @@
 import OpenAI from "openai";
+import { Pinecone } from "@pinecone-database/pinecone";
 
 // OpenAI config
 export const openai = new OpenAI({
   apiKey: "OPENAI_API_KEY",
   dangerouslyAllowBrowser: true,
 });
+
+// Pinecone config
+export const pinecone = new Pinecone({
+  apiKey: "PINECONE_API_KEY",
+});
+export const index = pinecone.index("my-content");
--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-import { openai } from "./config.js";
 import "./style.css";
+
+// Split text with LangChain
+
+// Generate embeddings
+
+// Write vectors into Pinecone index
+
+// Generate embedding from query
+
+// Query Pinecone using embedding
+// Log the reply to the console, or display it in your UI
